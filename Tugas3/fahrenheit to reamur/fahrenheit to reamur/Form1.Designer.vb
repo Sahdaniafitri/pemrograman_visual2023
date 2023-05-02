ed40f3673c21760a05bd4c14acb7ec9bf50891dc
Sahdaniafitri <103368891+Sahdaniafitri@users.noreply.github.com>
2023-05-02 17:36:43 +0700

    Add files via upload

---
+++ Tugas3/fahrenheit to reamur/fahrenheit to reamur/Form1.Designer.vb
@@ -0,0 +1,149 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtfahre = New System.Windows.Forms.TextBox()
+        Me.btnkonversi = New System.Windows.Forms.Button()
+        Me.txtreamur = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(206, 50)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(189, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Fahrenheit To Reamur"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(243, 104)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(96, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Fahrenheit"
+        '
+        'txtfahre
+        '
+        Me.txtfahre.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtfahre.Location = New System.Drawing.Point(224, 132)
+        Me.txtfahre.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtfahre.Name = "txtfahre"
+        Me.txtfahre.Size = New System.Drawing.Size(124, 26)
+        Me.txtfahre.TabIndex = 2
+        '
+        'btnkonversi
+        '
+        Me.btnkonversi.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnkonversi.Location = New System.Drawing.Point(238, 178)
+        Me.btnkonversi.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnkonversi.Name = "btnkonversi"
+        Me.btnkonversi.Size = New System.Drawing.Size(94, 31)
+        Me.btnkonversi.TabIndex = 3
+        Me.btnkonversi.Text = "Konversi"
+        Me.btnkonversi.UseVisualStyleBackColor = True
+        '
+        'txtreamur
+        '
+        Me.txtreamur.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtreamur.Location = New System.Drawing.Point(224, 261)
+        Me.txtreamur.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtreamur.Name = "txtreamur"
+        Me.txtreamur.Size = New System.Drawing.Size(124, 26)
+        Me.txtreamur.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(254, 233)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(72, 20)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Reamur"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Gadugi", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.MidnightBlue
+        Me.Label4.Location = New System.Drawing.Point(170, 354)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(237, 18)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Sahdania Fitri - 200511021 - TI20D"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Futura Md BT", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.SystemColors.InfoText
+        Me.Label5.Location = New System.Drawing.Point(332, 338)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(0, 16)
+        Me.Label5.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(588, 406)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtreamur)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnkonversi)
+        Me.Controls.Add(Me.txtfahre)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Name = "Form1"
+        Me.Text = "Konversi Suhu - Sahdania Fitri"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtfahre As TextBox
+    Friend WithEvents btnkonversi As Button
+    Friend WithEvents txtreamur As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+End Class
