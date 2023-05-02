f053a3f18141513c20ef438b4df16e068262fdcc
Sahdaniafitri <103368891+Sahdaniafitri@users.noreply.github.com>
2023-05-02 17:51:08 +0700

    Add files via upload

---
+++ Praktikum3/kerucut/kerucut/Form1.Designer.vb
@@ -0,0 +1,200 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtjari = New System.Windows.Forms.TextBox()
+        Me.txttinggi = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtluaspermukaan = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtvolume = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.btnhitung = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(41, 41)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(169, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Program Terstruktur"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(14, 84)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(83, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Jari - Jari"
+        '
+        'txtjari
+        '
+        Me.txtjari.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtjari.Location = New System.Drawing.Point(197, 84)
+        Me.txtjari.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtjari.Name = "txtjari"
+        Me.txtjari.Size = New System.Drawing.Size(94, 26)
+        Me.txtjari.TabIndex = 2
+        '
+        'txttinggi
+        '
+        Me.txttinggi.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txttinggi.Location = New System.Drawing.Point(197, 113)
+        Me.txttinggi.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txttinggi.Name = "txttinggi"
+        Me.txttinggi.Size = New System.Drawing.Size(94, 26)
+        Me.txttinggi.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(14, 113)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(57, 20)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Tinggi"
+        '
+        'txtluaspermukaan
+        '
+        Me.txtluaspermukaan.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtluaspermukaan.Location = New System.Drawing.Point(197, 227)
+        Me.txtluaspermukaan.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtluaspermukaan.Name = "txtluaspermukaan"
+        Me.txtluaspermukaan.Size = New System.Drawing.Size(94, 26)
+        Me.txtluaspermukaan.TabIndex = 8
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(14, 227)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(143, 20)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Luas Permukaan"
+        '
+        'txtvolume
+        '
+        Me.txtvolume.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtvolume.Location = New System.Drawing.Point(197, 197)
+        Me.txtvolume.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtvolume.Name = "txtvolume"
+        Me.txtvolume.Size = New System.Drawing.Size(94, 26)
+        Me.txtvolume.TabIndex = 6
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(14, 197)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(69, 20)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Volume"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Gadugi", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(264, 306)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(0, 19)
+        Me.Label6.TabIndex = 9
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Gadugi", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.ForeColor = System.Drawing.Color.MidnightBlue
+        Me.Label7.Location = New System.Drawing.Point(193, 388)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(267, 19)
+        Me.Label7.TabIndex = 10
+        Me.Label7.Text = "Sahdania Fitri - 200511021 - TI20D"
+        '
+        'btnhitung
+        '
+        Me.btnhitung.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnhitung.Location = New System.Drawing.Point(207, 153)
+        Me.btnhitung.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnhitung.Name = "btnhitung"
+        Me.btnhitung.Size = New System.Drawing.Size(70, 30)
+        Me.btnhitung.TabIndex = 11
+        Me.btnhitung.Text = "Hitung"
+        Me.btnhitung.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(598, 416)
+        Me.Controls.Add(Me.btnhitung)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtluaspermukaan)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtvolume)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txttinggi)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtjari)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Name = "Form1"
+        Me.Text = "Kerucut - Sahdania Fitri"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtjari As TextBox
+    Friend WithEvents txttinggi As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtluaspermukaan As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtvolume As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents btnhitung As Button
+End Class
