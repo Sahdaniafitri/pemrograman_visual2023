ed6a504ae011910f23313852ccc727a6ac7c888d
Sahdaniafitri <103368891+Sahdaniafitri@users.noreply.github.com>
2023-07-20 22:19:20 +0700

    Add files via upload

---
+++ PROJECT AKHIR/Aplikasi penyewaan baju pesta/Bajupesta.vb
@@ -0,0 +1,9 @@
+﻿Public Class Bajupesta
+    Public Property nps As String
+    Public Property nama As String
+    Public Property jns_kostum As String
+    Public Property tgl_sewaAs String
+    Public Property ukuran As String
+    Public Property harga As String
+
+End Class
