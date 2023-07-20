09168dc7f1a2a5c25ae39311da5f4ce71a09f43f
Sahdaniafitri <103368891+Sahdaniafitri@users.noreply.github.com>
2023-07-20 20:53:32 +0700

    Add files via upload

---
+++ Pertemuan10/MataKuliah_Client_Api_Sahdania/MataKuliah_Client_API/Form1.Designer.vb
@@ -0,0 +1,202 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtProdi = New System.Windows.Forms.TextBox()
+        Me.txtSKS = New System.Windows.Forms.TextBox()
+        Me.txtNamaMK = New System.Windows.Forms.TextBox()
+        Me.txtKodeMK = New System.Windows.Forms.TextBox()
+        Me.dgvData = New System.Windows.Forms.DataGridView()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        CType(Me.dgvData, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(220, 480)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(297, 20)
+        Me.Label5.TabIndex = 37
+        Me.Label5.Text = "Sahdania Fitri_200511021_TIF20D"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(87, 152)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(41, 17)
+        Me.Label4.TabIndex = 33
+        Me.Label4.Text = "Prodi"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(87, 106)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(35, 17)
+        Me.Label3.TabIndex = 32
+        Me.Label3.Text = "SKS"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(87, 65)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(117, 17)
+        Me.Label2.TabIndex = 31
+        Me.Label2.Text = "Nama Matakuliah"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(87, 28)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(113, 17)
+        Me.Label1.TabIndex = 30
+        Me.Label1.Text = "Kode Matakuliah"
+        '
+        'txtProdi
+        '
+        Me.txtProdi.Location = New System.Drawing.Point(212, 143)
+        Me.txtProdi.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtProdi.Name = "txtProdi"
+        Me.txtProdi.Size = New System.Drawing.Size(161, 22)
+        Me.txtProdi.TabIndex = 29
+        '
+        'txtSKS
+        '
+        Me.txtSKS.Location = New System.Drawing.Point(211, 102)
+        Me.txtSKS.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtSKS.Name = "txtSKS"
+        Me.txtSKS.Size = New System.Drawing.Size(163, 22)
+        Me.txtSKS.TabIndex = 28
+        '
+        'txtNamaMK
+        '
+        Me.txtNamaMK.Location = New System.Drawing.Point(211, 62)
+        Me.txtNamaMK.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtNamaMK.Name = "txtNamaMK"
+        Me.txtNamaMK.Size = New System.Drawing.Size(163, 22)
+        Me.txtNamaMK.TabIndex = 27
+        '
+        'txtKodeMK
+        '
+        Me.txtKodeMK.Location = New System.Drawing.Point(211, 25)
+        Me.txtKodeMK.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtKodeMK.Name = "txtKodeMK"
+        Me.txtKodeMK.Size = New System.Drawing.Size(163, 22)
+        Me.txtKodeMK.TabIndex = 26
+        '
+        'dgvData
+        '
+        Me.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvData.Location = New System.Drawing.Point(56, 190)
+        Me.dgvData.Margin = New System.Windows.Forms.Padding(4)
+        Me.dgvData.Name = "dgvData"
+        Me.dgvData.Size = New System.Drawing.Size(608, 265)
+        Me.dgvData.TabIndex = 25
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDelete.Location = New System.Drawing.Point(454, 102)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(125, 32)
+        Me.btnDelete.TabIndex = 40
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSubmit.Location = New System.Drawing.Point(454, 19)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(125, 32)
+        Me.btnSubmit.TabIndex = 39
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(454, 59)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(125, 32)
+        Me.btnClear.TabIndex = 38
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Khaki
+        Me.ClientSize = New System.Drawing.Size(721, 525)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtProdi)
+        Me.Controls.Add(Me.txtSKS)
+        Me.Controls.Add(Me.txtNamaMK)
+        Me.Controls.Add(Me.txtKodeMK)
+        Me.Controls.Add(Me.dgvData)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgvData, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtProdi As TextBox
+    Friend WithEvents txtSKS As TextBox
+    Friend WithEvents txtNamaMK As TextBox
+    Friend WithEvents txtKodeMK As TextBox
+    Friend WithEvents dgvData As DataGridView
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents btnClear As Button
+End Class
