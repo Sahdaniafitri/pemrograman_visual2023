229466a23fa5501c124ee56c7cfe3c78fbf32bcc
Sahdaniafitri <103368891+Sahdaniafitri@users.noreply.github.com>
2023-07-21 19:44:03 +0700

    Delete PROJECT AKHIR/Aplikasi penyewaan baju pesta directory

--- PROJECT AKHIR/Aplikasi penyewaan baju pesta/Bajupesta.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Bajupesta
-    Public Property nps As String
-    Public Property nama As String
-    Public Property jns_kostum As String
-    Public Property tgl_sewaAs String
-    Public Property ukuran As String
-    Public Property harga As String
-
-End Class
